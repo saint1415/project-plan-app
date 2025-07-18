172ee7befd9c76dd794ce8126c28255bc6ee593e
saint1415 <saint1415@gmail.com>
2025-07-18 15:48:36 -0400

    Update index.js

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,1 +1,14 @@
+// Utility Functions
+// TODO: Implement helper functions
 
+export const exportToPDF = () => {
+  // TODO: Implement PDF export
+};
+
+export const exportToWord = () => {
+  // TODO: Implement Word export
+};
+
+export const copyToClipboard = (text) => {
+  // TODO: Implement clipboard functionality
+};
