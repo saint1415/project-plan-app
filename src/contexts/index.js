3555de23d53e9ed8b7e1d027d154b9e1593d69ea
saint1415 <saint1415@gmail.com>
2025-07-18 15:49:02 -0400

    Update index.js

--- src/contexts/index.js
+++ src/contexts/index.js
@@ -1,1 +1,6 @@
+// React Contexts for State Management
+// TODO: Implement contexts for user, plan, settings, etc.
 
+export { default as UserContext } from './UserContext';
+export { default as PlanContext } from './PlanContext';
+export { default as SettingsContext } from './SettingsContext';
