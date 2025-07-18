59d2253aa0fc4c78393f2b9df8fcd4cbd1fe4d51
saint1415 <saint1415@gmail.com>
2025-07-18 15:48:21 -0400

    Update index.js

--- src/services/index.js
+++ src/services/index.js
@@ -1,1 +1,6 @@
+// Service Layer for External Integrations
+// TODO: Implement service abstractions
 
+export { default as AIService } from './AIService';
+export { default as CloudStorageService } from './CloudStorageService';
+export { default as AnalyticsService } from './AnalyticsService';
