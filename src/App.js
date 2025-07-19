fcab550b30d787970438d1feba5f552b4f54d4d0
saint1415 <saint1415@gmail.com>
2025-07-19 00:35:29 -0400

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ fontFamily: "sans-serif", textAlign: "center", marginTop: "3rem" }}>
+      <h1>Welcome to Project Plan Pro</h1>
+      <p>
+        Your React app is running!<br />
+        Replace this content with your project planner features.
+      </p>
+    </div>
+  );
+}
+
+export default App;
