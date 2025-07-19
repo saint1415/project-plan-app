2f6988eaf88e5e43730cc1542a1d4ccdc96cf9c4
saint1415 <saint1415@gmail.com>
2025-07-19 00:05:26 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-// React App Entry Point
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './styles.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
